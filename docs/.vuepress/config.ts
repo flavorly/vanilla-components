44db7f67d51612628bebf03d82f6adf58bb71fcd
igerslike <support@igerslike.com>
2021-11-24 10:57:06 +0000

    Vite attempt 9991

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -49,25 +49,24 @@ export default {
       },
     ],
   ],
-  // when using vuepress package, you can omit this field
-  // because webpack is the default bundler
   bundler: '@vuepress/bundler-vite',
-  // options for webpack bundler
   bundlerConfig: {
-      // chainWebpack(config,isServer,isBuild){
-      //   //console.log(config);
-      //   config.resolve.alias.set('@', path.resolve(__dirname, "./../../src"))
-      // },
       viteOptions: {
         resolve: {
           alias: [
-            //{ find: '/^~/', replacement: ''},
             { find: '@', replacement: path.resolve(__dirname,'./../../src') }
           ]
-        }
+        },
+        css: {
+          postcss: {
+            plugins: [
+              require('postcss-import'),
+              require('tailwindcss/nesting'),
+              require('tailwindcss'),
+              require('autoprefixer'),
+            ]
+          }
+        },
       }
   },
-  // alias: {
-  //   '@': path.resolve(__dirname,'./../../src'),
-  // },
 }
