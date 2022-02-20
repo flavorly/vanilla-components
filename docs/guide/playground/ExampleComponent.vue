68c4e5c74360957f5f2995860945d90d1aa4511f
jon <support@indigit.pt>
2022-02-20 20:39:19 +0000

    Updated datetimepicker

--- docs/guide/playground/ExampleComponent.vue
+++ docs/guide/playground/ExampleComponent.vue
@@ -1,18 +1,18 @@
-<template>
-  <div class="space-x-2 space-y-2 flex items-center mx-auto flex-col items-center justify-start sm:space-y-0 sm:flex-row sm:items-end sm:justify-around">
-    <vanilla-flags
-      :country="'pt'"
-      class="inline-flex"
-    />
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="space-x-2 space-y-2 flex items-center mx-auto flex-col items-center justify-start sm:space-y-0 sm:flex-row sm:items-end sm:justify-around">-->
+<!--    <vanilla-flags-->
+<!--      :country="'pt'"-->
+<!--      class="inline-flex"-->
+<!--    />-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-import { VanillaFlags } from "@/index";
-export default {
-  name: "ExampleComponent",
-  components: {
-    VanillaFlags
-  },
-}
-</script>
+<!--<script>-->
+<!--import { VanillaFlags } from "@/index";-->
+<!--export default {-->
+<!--  name: "ExampleComponent",-->
+<!--  components: {-->
+<!--    VanillaFlags-->
+<!--  },-->
+<!--}-->
+<!--</script>-->
