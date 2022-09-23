a73d86537fadafbc784c584a0d389865e1c9ceef
Jon <support@indigit.pt>
2022-09-24 00:51:56 +0100

    Phone Input

--- packages/vanilla-components/src/components/phone-input/config.ts
+++ packages/vanilla-components/src/components/phone-input/config.ts
@@ -1,11 +1,30 @@
-export const VanillaPhoneNumberConfig = {
+import type { CountryCode } from 'libphonenumber-js/types'
+import type { InputHTMLAttributes } from 'vue'
+import type { Data, WithVariantPropsAndClassesList } from '@/core/types'
+
+export const phoneInputConfig = {
   classes: {
     wrapper: 'relative',
     input: 'rounded-t-none focus:z-20 pl-14',
   },
 }
 
-export const VanillaPhoneNumberClassesKeys = Object.keys(VanillaPhoneNumberConfig.classes)
+export const phoneInputClassesKeys = Object.keys(phoneInputConfig.classes)
 
-export type VanillaPhoneNumberClassesValidKeys = keyof typeof VanillaPhoneNumberConfig.classes
+export declare type PhoneInputClassesValidKeys = keyof typeof phoneInputConfig.classes
+
+export declare type PhoneInputProps = WithVariantPropsAndClassesList<{
+  modelValue?: string | null
+  countryCode?: CountryCode
+  phonePlaceholder?: string | undefined
+  countryPlaceholder?: string | undefined
+  favoriteCountries?: string | undefined
+  placeholder?: string
+  searchBoxPlaceholder?: string
+  noResultsText?: string
+  searchingText?: string
+  loadingClosedPlaceholder?: string
+  loadingMoreResultsText?: string
+  minimumInputLengthText?: string
+} & InputHTMLAttributes & Data, PhoneInputClassesValidKeys>
 
--- packages/vanilla-components/src/components/phone-input/phone-input.vue
+++ packages/vanilla-components/src/components/phone-input/phone-input.vue
@@ -1,154 +1,129 @@
-<script lang="ts">
+<script setup lang="ts">
 import type { PropType, Ref } from 'vue'
 import type { CountryCallingCode, CountryCode, PhoneNumber } from 'libphonenumber-js/types'
-import { defineComponent, ref, watch } from 'vue'
+import { ref, watch } from 'vue'
 import { getCountryCallingCode, parsePhoneNumber } from 'libphonenumber-js'
-import type { VanillaFavoriteCountriesValue } from '../SelectCountry'
-import { useBootVariant, useConfigurationWithClassesList, useVModel, useVariantProps } from '../../core'
-import VanillaSelectCountry from '../SelectCountry/SelectCountry.vue'
-import VanillaFormErrors from '../FormErrors/FormErrors.vue'
-import VanillaFormFeedback from '../FormFeedback/form-feedback.vue'
-import type { MinimumInputLengthTextProp } from '../RichSelect'
-import VanillaInput from '../input/input.vue'
-import { VanillaPhoneNumberClassesKeys, VanillaPhoneNumberConfig } from './phone-input.vue'
-import type { VanillaPhoneNumberProps } from './phone-input.vue'
+import { phoneInputClassesKeys, phoneInputConfig } from './config'
+import type { PhoneInputClassesValidKeys, PhoneInputProps } from './config'
+import CountryInput from '@/components/country-input/country-input.vue'
+import FormErrors from '@/components/forms/form-errors.vue'
+import FormFeedback from '@/components/forms/form-feedback.vue'
+import type { MinimumInputLengthTextProp } from '@/core/types'
+import Input from '@/components/input/input.vue'
+import { useBootVariant, useConfiguration, useVModel, useVariantProps } from '@/core/use'
+import type { FavoriteCountriesValue } from '@/components/country-input/config'
 
-export default defineComponent({
-    components: {
-        VanillaInput,
-        VanillaSelectCountry,
-        VanillaFormErrors,
-        VanillaFormFeedback,
-    },
-    props: {
-        ...useVariantProps<VanillaPhoneNumberProps>(),
-        modelValue: {
-            type: [String, Number] as PropType<string>,
-            default: undefined,
-        },
-        countryCode: {
-            type: [String] as PropType<CountryCode>,
-            default: '',
-        },
-        phonePlaceholder: {
-            type: [String, Number] as PropType<string | number>,
-            default: 'Your national number',
-        },
-        countryPlaceholder: {
-            type: [String, Number] as PropType<string | number>,
-            default: 'Select your phone indicative',
-        },
-        favoriteCountries: {
-            type: [Array, Object, undefined] as PropType<VanillaFavoriteCountriesValue>,
-            required: false,
-            default: () => ['US', 'GB', 'PT', 'FR', 'DE'],
-        },
-        searchBoxPlaceholder: {
-            type: String,
-            default: 'Your Phone Number country',
-        },
-        noResultsText: {
-            type: String,
-            default: '😟 Sorry but we did not find any countries to your query. Try another one query?',
-        },
-        searchingText: {
-            type: String,
-            default: 'Please wait, searching for results...',
-        },
-        loadingClosedPlaceholder: {
-            type: String,
-            default: 'Loading, please wait ...',
-        },
-        loadingMoreResultsText: {
-            type: String,
-            default: 'Loading more options...',
-        },
-        minimumInputLengthText: {
-            type: [Function, String] as PropType<MinimumInputLengthTextProp>,
-            default: () => (minimumInputLength: number): string => `Search more countries by entering ${minimumInputLength} or more characters`,
-        },
-    },
-    emits: [
-        'update:countryDialCode',
-        'update:countryCode',
-        'update:countryName',
-        'update:modelValue',
-        'update:phoneNumberNational',
-        'update:phoneNumberInternational',
-    ],
-    setup(props, { emit }) {
-        const localRefPhone = ref(null)
-        const localRefCountry = ref(null)
-        const localValue = useVModel(props, 'modelValue')
-        const phoneCountryCode: CountryCode | Ref = ref(props.countryCode)
-        const phoneDialCode: CountryCallingCode | Ref = ref(null)
-        const phoneNumber: Ref<string | undefined> = ref(localValue.value)
-        const isValidPhoneNumber: Ref<boolean> = ref(false)
-        const parsedPhoneNumber: Ref<PhoneNumber> | Ref = ref(null)
-
-        const attemptToParseNumber = (phoneNumberValue: string | undefined, phoneCountryCodeValue: CountryCode) => {
-            try {
-                // Always resolve the country code to dial code
-                phoneDialCode.value = getCountryCallingCode(phoneCountryCodeValue)
-
-                // Attempt to parse the number and country code
-                parsedPhoneNumber.value = parsePhoneNumber(phoneNumberValue || '', phoneCountryCodeValue)
-
-                // If we are able to parse, then we will assign the new values
-                if (parsedPhoneNumber.value) {
-                    isValidPhoneNumber.value = parsedPhoneNumber.value?.isValid()
-                    phoneCountryCode.value = parsedPhoneNumber.value.country
-                    phoneDialCode.value = parsedPhoneNumber.value?.countryCallingCode
-                    phoneNumber.value = parsedPhoneNumber.value?.nationalNumber
-                    localValue.value = `+${phoneDialCode.value}${phoneNumber.value}`
-                }
-            }
-            catch (e) {
-                localValue.value = `+${phoneDialCode.value}${phoneNumber.value}`
-            }
-        }
-
-        attemptToParseNumber(phoneNumber.value, phoneCountryCode.value)
-
-        // Only boot here, first we must prepare the values before start to watch.
-        const {
-            hasErrors,
-            localErrors,
-            localVariant,
-        } = useBootVariant(props, 'errors', localValue)
-
-        const { configuration } = useConfigurationWithClassesList<VanillaPhoneNumberProps>(
-            VanillaPhoneNumberConfig,
-            VanillaPhoneNumberClassesKeys,
-            localVariant,
-        )
-
-        // Watch Country Code and Phone number together
-        // When one changes we will trigger the model value & emit back
-        watch([phoneCountryCode, phoneNumber], ([newPhoneCountryCode, newPhoneNumber]) => {
-            attemptToParseNumber(newPhoneNumber, newPhoneCountryCode)
-
-            // Watch & Emit additional data
-            emit('update:countryDialCode', phoneDialCode.value)
-            emit('update:countryCode', phoneCountryCode.value)
-            emit('update:phoneNumberNational', phoneNumber.value)
-            emit('update:phoneNumberInternational', localValue.value)
-        }, { immediate: true })
-
-        return {
-            configuration,
-            localRefPhone,
-            localRefCountry,
-            localVariant,
-            localErrors,
-            hasErrors,
-            phoneCountryCode,
-            phoneNumber,
-            phoneDialCode,
-            isValidPhoneNumber,
-        }
-    },
+const props = defineProps({
+  ...useVariantProps<PhoneInputProps, PhoneInputClassesValidKeys>(),
+  modelValue: {
+    type: [String, Number] as PropType<string>,
+    default: undefined,
+  },
+  countryCode: {
+    type: [String] as PropType<CountryCode>,
+    default: '',
+  },
+  phonePlaceholder: {
+    type: [String, Number] as PropType<string | number>,
+    default: 'Your national number',
+  },
+  countryPlaceholder: {
+    type: [String, Number] as PropType<string | number>,
+    default: 'Select your phone indicative',
+  },
+  favoriteCountries: {
+    type: [Array, Object, undefined] as PropType<FavoriteCountriesValue>,
+    required: false,
+    default: () => ['US', 'GB', 'PT', 'FR', 'DE'],
+  },
+  searchBoxPlaceholder: {
+    type: String,
+    default: 'Your Phone Number country',
+  },
+  noResultsText: {
+    type: String,
+    default: '😟 Sorry but we did not find any countries to your query. Try another one query?',
+  },
+  searchingText: {
+    type: String,
+    default: 'Please wait, searching for results...',
+  },
+  loadingClosedPlaceholder: {
+    type: String,
+    default: 'Loading, please wait ...',
+  },
+  loadingMoreResultsText: {
+    type: String,
+    default: 'Loading more options...',
+  },
+  minimumInputLengthText: {
+    type: [Function, String] as PropType<MinimumInputLengthTextProp>,
+    default: () => (minimumInputLength: number): string => `Search more countries by entering ${minimumInputLength} or more characters`,
+  },
 })
+
+const emit = defineEmits([
+  'update:countryDialCode',
+  'update:countryCode',
+  'update:countryName',
+  'update:modelValue',
+  'update:phoneNumberNational',
+  'update:phoneNumberInternational',
+])
+
+const localRefPhone = ref(null)
+const localRefCountry = ref(null)
+const localValue = useVModel(props, 'modelValue')
+const phoneCountryCode: CountryCode | Ref = ref(props.countryCode)
+const phoneDialCode: CountryCallingCode | Ref = ref(null)
+const phoneNumber: Ref<string | undefined> = ref(localValue.value)
+const isValidPhoneNumber: Ref<boolean> = ref(false)
+const parsedPhoneNumber: Ref<PhoneNumber> | Ref = ref(null)
+
+const attemptToParseNumber = (phoneNumberValue: string | undefined, phoneCountryCodeValue: CountryCode) => {
+  try {
+    // Always resolve the country code to dial code
+    phoneDialCode.value = getCountryCallingCode(phoneCountryCodeValue)
+
+    // Attempt to parse the number and country code
+    parsedPhoneNumber.value = parsePhoneNumber(phoneNumberValue || '', phoneCountryCodeValue)
+
+    // If we are able to parse, then we will assign the new values
+    if (parsedPhoneNumber.value) {
+      isValidPhoneNumber.value = parsedPhoneNumber.value?.isValid()
+      phoneCountryCode.value = parsedPhoneNumber.value.country
+      phoneDialCode.value = parsedPhoneNumber.value?.countryCallingCode
+      phoneNumber.value = parsedPhoneNumber.value?.nationalNumber
+      localValue.value = `+${phoneDialCode.value}${phoneNumber.value}`
+    }
+  }
+  catch (e) {
+    localValue.value = `+${phoneDialCode.value}${phoneNumber.value}`
+  }
+}
+
+attemptToParseNumber(phoneNumber.value, phoneCountryCode.value)
+
+// Only boot here, first we must prepare the values before start to watch.
+const {
+  hasErrors,
+  localErrors,
+  localVariant,
+} = useBootVariant(props, 'errors', localValue)
+
+const { configuration } = useConfiguration<PhoneInputProps>(phoneInputConfig, phoneInputClassesKeys)
+
+// Watch Country Code and Phone number together
+// When one changes we will trigger the model value & emit back
+watch([phoneCountryCode, phoneNumber], ([newPhoneCountryCode, newPhoneNumber]) => {
+  attemptToParseNumber(newPhoneNumber, newPhoneCountryCode)
+
+  // Watch & Emit additional data
+  emit('update:countryDialCode', phoneDialCode.value)
+  emit('update:countryCode', phoneCountryCode.value)
+  emit('update:phoneNumberNational', phoneNumber.value)
+  emit('update:phoneNumberInternational', localValue.value)
+}, { immediate: true })
 </script>
 
 <template>
@@ -157,7 +132,7 @@ export default defineComponent({
     :class="configuration.classesList.wrapper"
   >
     <div>
-      <VanillaSelectCountry
+      <CountryInput
         ref="localRefCountry"
         v-model="phoneCountryCode"
         :favorite-countries="favoriteCountries"
@@ -176,7 +151,7 @@ export default defineComponent({
         :loading-closed-placeholder="loadingClosedPlaceholder"
         :loading-more-results-text="loadingMoreResultsText"
       />
-      <VanillaInput
+      <Input
         ref="localRefPhone"
         v-model="phoneNumber"
         :variant="localVariant"
@@ -198,13 +173,13 @@ export default defineComponent({
             >+{{ phoneDialCode }}</span>
           </slot>
         </template>
-      </VanillaInput>
+      </Input>
     </div>
     <slot
       name="errors"
       v-bind="{ hasErrors, localErrors }"
     >
-      <VanillaFormErrors
+      <FormErrors
         v-if="hasErrors && showErrors"
         :errors="localErrors"
       />
@@ -213,7 +188,7 @@ export default defineComponent({
       name="feedback"
       v-bind="{ hasErrors, feedback }"
     >
-      <VanillaFormFeedback
+      <FormFeedback
         v-if="!hasErrors && feedback !== undefined && showFeedback"
         :text="feedback"
       />
